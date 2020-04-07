b78f245f6667a6c6e54c9b2f765ada00326171b3
Steven <stevenberrisford@gmail.com>
2020-04-07 18:00:40 +0100

    added submit functionality to main.js

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');//Decode the body HHTTP request
 const cors = require('cors')
 
-
-
 const PORT = 8080;
 const app = express();
 app.use(cors());
@@ -18,21 +16,29 @@ app.set('views', __dirname + '/views');
 app.engine('html', require('ejs').renderFile);
 
 
-//let posts = [{"title": "test title"}]
-let posts = []
+// //let posts = [{"title": "test title"}]
+// let posts = []
+// app.post('/submissions', (req, res) => {
+//     let data = req.body;
+//     console.log(data);
+//     posts.push(data);
+//     res.end()
+// })
+// app.get('/', (req, res) => {
+//     res.render('index', { posts })
+// })
 app.post('/submissions', (req, res) => {
-    let data = req.body;
-    console.log(data);
-    posts.push(data);
-    res.end()
-})
-app.get('/', (req, res) => {
-    res.render('index', { posts })
-})
-app.get('/submissions', (req, res) => {
-    console.log(posts)
+    console.log(req.body);
+    // console.log(posts);
     //res.render('index', {posts})
-    res.send(posts)
+    res.send(req.body);
+})
+
+app.post('/comments', (req, res) => {
+    console.log(req.body);
+    // console.log(posts);
+    //res.render('index', {posts})
+    res.send(req.body);
 })
 
 app.listen(PORT, () => console.log(`I love my beer and app listening on port ${PORT}!`))
--- public/main.js
+++ public/main.js
@@ -1,17 +1,70 @@
-//Send data from the form to back end
-console.log('testing')
-function fetchPosts(){
-    fetch('/submissions', {method: 'GET'})
-    .then(resp => resp.json())
-    .then(renderPosts)
-    //.then( posts => console.log(posts))
-    //console.log(posts)
-    }
-    const renderPosts = (posts) => {
-    console.log(posts)
-    const x = `${posts.data}`;
-    document.getElementsByClassName('post-title').innerHTML = x;
-    // if ( window.history.replaceState ) {
-    // window.history.replaceState( null, null, window.location.href );
-    }
-    fetchPosts();
+try {
+    let idArray = document.idArray;
+} catch {
+    let idArray = [1, 2, 3, 4, 5];
+};
+
+$(document).ready(() => {
+    // LISTEN FOR BUTTON CLICK ON SUBMIT
+    $("#submit").click(function(event) {
+        event.preventDefault();
+        // GET VALUES FROM SUBMISSION FORM
+        let submission = {
+            id: null,
+            title: $("#title").val(),
+            message: $("#message").val(),
+            gif: $("#gif").val(),
+            dateTime: (new Date()).getTime(),
+            commentArray: [1, 1]
+        };
+        console.log(submission);
+        // POSTS SUBMISSION TO SERVER
+        $.post('http://localhost:8080/submissions', submission, function(data) {
+            // DO STUFF
+            console.log(data);
+            
+
+
+        });
+    });
+
+    // LISTEN FOR BUTTON CLICK ON COMMENT
+    $("#comment1").click(function(event) {
+        event.preventDefault();
+        commentFunction(1);
+    });
+    $("#comment2").click(function(event) {
+        event.preventDefault();
+        commentFunction(2);
+    });
+    $("#comment3").click(function(event) {
+        event.preventDefault();
+        commentFunction(3);
+    });
+    $("#comment4").click(function(event) {
+        event.preventDefault();
+        commentFunction(4);
+    });
+    $("#comment5").click(function(event) {
+        event.preventDefault();
+        commentFunction(5);
+    });
+});
+
+function commentFunction (number) {
+    event.preventDefault();
+    // GET VALUES FROM COMMENT FORM
+    let comment = {
+        id: idArray[number-1],
+        message: $(`#textarea${number}`).val(),
+        dateTime: (new Date()).getTime()
+    };
+    // POSTS COMMENT TO SERVER
+    $.post('http://localhost:8080/comments', comment, function(data) {
+        // DO STUFF
+
+        console.log(data);
+
+
+
+    })};

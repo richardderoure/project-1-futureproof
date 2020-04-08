ce2220feba189b0dc8b2f958f86c22ff7022b608
richard <r.d.deroure@se16.qmul.ac.uk>
2020-04-08 10:05:23 +0100

    working back-end, added getTime functionality

--- app.js
+++ app.js
@@ -4,182 +4,108 @@ const PORT = process.env.PORT || 8000;
 const fs = require('fs');
 var bodyParser = require('body-parser');
 
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({
+    extended: false
+}));
 app.use(express.static("public"));
 
-let database = {test:[{"test":"test","test":"test"}]};
-
-// let comment={
-//     text: "a",
-//     emoji: "b",
-//     dateTime: "01/01/20",
-//     id: "second"
-// };
-
-fs.writeFile('database.json','{"submissions":[]}', (err) => {
-    if (err) { console.log('error saving to file');
-    return
-};
+//Writes default file structure ready to receive daya
+fs.writeFile('database.json', '{"submissions":[]}', (err) => {
+    if (err) {
+        console.log('error saving to file');
+        return
+    };
     console.log('Default file setup complete');
 });
 
-let submission={
+//Testing submission format (this is format we will recieve data from front end) - REPLACE INSTANCES OF submission WITH req.body IN PRODUCTION CODE.
+let submission = {
     id: 0,
-    title:"This is a test!",
+    title: "This is a test!",
     message: "this is testing our placeholder submission on the back end",
-    gif:"gif link here!",
-    dateTime: "01/01/20",
-    commentArray: ['comment test 1','comment test 2']
-};
-let comment={
-    id: 1,
-    message: 'THIS IS A COMMENT',
-    dateTime: "01/01/20"
+    gif: "gif link here!",
+    dateTime: "",
+    commentArray: []
 };
 
-app.get('/', (req,res) => {
+//Testing comment format (this is format we will receive data from front end) - REPLACE INSTANCES OF comment WITH req.body IN PRODUCTION CODE.
+let comment = {
+    id: 1,
+    message: 'THIS IS A COMMENT',
+    dateTime: ""
+};
+
+//Renders home page
+app.get('/', (req, res) => {
     app.render('index.html');
 });
 
+//Gets data from form submission at the route /submission
+app.get('/submission', (req, res) => {
 
-app.get('/submission', (req,res) => {
-    
-    fs.readFile('./database.json','utf8', (err,data)=>{
-        if(err){
+    fs.readFile('./database.json', 'utf8', (err, data) => {
+        if (err) {
             console.log('error reading file!');
-            return 
-        };
-        console.log('Reading from file...');
-        let dataJson = JSON.parse(data);
-        dataJson.submissions.push(submission);
-        
-        //Assign ID to each object
-        for(let i = 0; i < dataJson.submissions.length - 1; i++){
-            console.log('length is: ' + dataJson.submissions.length);
-            dataJson.submissions[i]['id'] = i;
-        };
-
-        //Write JSON to database file
-        let myJson=JSON.stringify(dataJson);
-        fs.writeFile('database.json',myJson, (err) => {
-            if (err) { console.log('error saving to file');
             return
         };
-            console.log('Saved to file!');
+        console.log('Reading from file...');
+
+        let dataJson = JSON.parse(data);
+        getTime(submission);
+
+        //Assign ID in one line
+        submission.id = dataJson.submissions.length;
+        dataJson.submissions.push(submission);
+
+        //Write JSON to database file
+        let myJson = JSON.stringify(dataJson);
+        fs.writeFile('database.json', myJson, (err) => {
+            if (err) {
+                console.log('error saving to file');
+                return
+            };
+            console.log('Saved submission to file!');
+        });
+
+        res.send(myJson);
+    });
+});
+
+//Gets data from comments on posts at the route /comments
+app.get('/comments', (req, res) => {
+    fs.readFile('./database.json', 'utf8', (err, data) => {
+        if (err) {
+            console.log('error reading comment!');
+            return
+        };
+        console.log('Reading comment...');
+
+        let dataJson = JSON.parse(data);
+        getTime(comment)
+        dataJson.submissions[comment.id].commentArray.push(comment);
+
+        //Write JSON to database file
+        let myJson = JSON.stringify(dataJson);
+        fs.writeFile('database.json', myJson, (err) => {
+            if (err) {
+                console.log('error saving to file');
+                return
+            };
+            console.log('Saved comment to file!');
         });
         
         res.send(myJson);
     });
 });
 
-app.get('/comments', (req,res) =>{
-    fs.readFile('./database.json','utf8', (err,data)=>{
-        if(err){
-            console.log('error reading comment!');
-            return 
-        };
-        console.log('Reading comment...');
-        let dataJson = JSON.parse(data);
-        dataJson.submissions[comment.id].commentArray.push(comment.message);
-  
-        //Write JSON to database file
-        let myJson=JSON.stringify(dataJson);
-        fs.writeFile('database.json',myJson, (err) => {
-            if (err) { console.log('error saving to file');
-            return
-        };
-            console.log('Saved to file!');
-        });
-        
-        res.send(myJson);
-    });
-
-})
-
-
-
-
-//idCounter++;
-
-// let myJson=JSON.stringify(dataJson);
-        // j=dataJson.test[1].submission[0]['id'];
-        // j=Number(j);
-        // console.log(Number(j));
-        // j=j+1;
-        // console.log(j);
-        // j=j.toString();
-        // console.log(typeof j);
-
-//app.get('/comment', (req,res) => {
-
-    
-
-//});
-
-
-
-
-
+//Function to get current data and time. Takes comment or submission as parameter, and updates dateTime value.
+const getTime = (object) => {
+    var today = new Date();
+    var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    object.dateTime = date + ' ' + time;
+};
 
 app.listen(PORT, () => {
     console.log(`Listening on port: ${PORT}`);
 });
-
-    
-    
-        
-//         var idArray=[];
-//         for(var i=0; i<database.length; i++){
-//             idArray[i]=Number(database[i]); //Try to replace with i or database[i].id;
-
-//         };
-//         var maxId=Math.max(idArray);
-//         //console.log(maxId);
-//         database[maxId]=submission; //req.body...
-//     }catch(e){
-//         database[0]=submission; //database.push(submission)
-//     };
-
-    
-  
-//     fs.writeFile('database.txt',database,function (err) {
-//   if (err) throw err;
-//   console.log('Saved!');})
-//     //var topDatabase={};
-    
-    
-//     res.send(database);
-
-// });
-// //app.get('/comment', function(req,res){
-
-
-
-
-
-  /*var dateTimes = [];
-var dateTimeIndices = [];
-for (var i=0;(i<Object.keys(database).length);i++){
-    dateTimes[i]=database[i].dateTime;
-    dateTimeIndices[i]=i;
-};
-console.log(dateTimeIndices);
-//1) combine the arrays:
-var list = [];
-for (var j = 0; j < dateTimes.length; j++) 
-    list.push({'dateTime': dateTimes[j], 'dateTimeIndex': dateTimeIndices[j]});
-//2) sort:
-list.sort(function(a, b) {
-    return ((a.dateTime < b.dateTime) ? -1 : ((a.dateTime == b.dateTime) ? 0 : 1));
-    //Sort could be modified to, for example, sort on the dateTimeIndex 
-    // if the dateTime is the same.
-});
-//3) separate them back out:
-for (var k = 0; k < list.length; k++) {
-    dateTimes[k] = list[k].dateTime;
-    dateTimeIndices[k] = list[k].dateTimeIndex;
-};
-console.log(dateTimeIndices);
-   */
-
-   //{"test":[{"test":"test"}]}
